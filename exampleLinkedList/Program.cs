11d68db2cb483418e0629fcce530e6faf94984da
user <user@PC>
2023-03-12 19:23:04 -0400

    Add project files.

---
+++ exampleLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exampleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> newList = new LinkedList<string>();
+            // add items
+            newList.AddLast("A");
+            newList.AddLast("B");
+            newList.AddLast("C");
+            newList.AddLast("D");
+            newList.AddLast("E");
+            // display that first node
+            string[] listArray = new string[newList.Count];
+            newList.CopyTo(listArray, 0);
+
+            Console.WriteLine(listArray[0]);
+            // now the last
+            Console.WriteLine(listArray[listArray.Length-1]);
+            // add another item in the middle
+            newList.AddAfter(newList.Find("B"), "F");
+            // kill a node
+            newList.RemoveLast();
+            // count those NODES!
+            Console.WriteLine(newList.Count);
+            // display those NODES!
+            foreach (string s in newList) Console.WriteLine(s);
+        }
+    }
+}
